9d5d19eb699d48b6d2acadfd029df1de44b1895b
ZhangKuoKuo <KKK1@chenzongbo.local>
2016-03-04 09:41:30 +0800

    Initial Commit

---
+++ MyFirstIten/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstIten
+//
+//  Created by KKK on 16/3/4.
+//  Copyright © 2016年 KKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstIten/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstIten
+//
+//  Created by KKK on 16/3/4.
+//  Copyright © 2016年 KKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
